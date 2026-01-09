c0d10359fb4b66592bb4adfa4b15226a37b398de
nikvekariya02 <vekariyanikhil04@gmail.com>
2026-01-09 13:42:19 +0530

    chore: raise chunk size warning limit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Increase chunk size warning threshold for production builds
+    chunkSizeWarningLimit: 1000, // in kBs
+  },
 })
